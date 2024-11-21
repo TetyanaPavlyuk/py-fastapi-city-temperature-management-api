ac79d056520a9f1de06e995215e719ee73c8f48e
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-11-21 13:56:34 +0200

    Create schemas.py for City app

---
+++ city/schemas.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from pydantic  import BaseModel
+
+
+class CityBase(BaseModel):
+    name: str
+    additional_info: str
+
+
+class CityCreate(CityBase):
+    pass
+
+
+class City(CityBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class CityUpdate(CityBase):
+    name: Optional[str] = None
+    additional_info: Optional[str] = None
+
+    class Config:
+        from_attributes = True
+
+
+class CityDelete(BaseModel):
+    message: str
+    deleted_id: int
