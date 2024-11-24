a3f60a5e6d45c0b5d302f661bc483abf97a6dcde
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-11-24 19:10:12 +0200

    Edit endpoints for city app - delete last slashes

--- city/router.py
+++ city/router.py
@@ -8,22 +8,22 @@ from city import crud, schemas
 router = APIRouter()
 
 
-@router.post("/cities/", response_model=schemas.City)
+@router.post("/cities", response_model=schemas.City)
 async def create_city(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
     return await crud.post_city(db=db, city=city)
 
 
-@router.get("/cities/", response_model=list[schemas.City])
+@router.get("/cities", response_model=list[schemas.City])
 async def list_cities(db: AsyncSession = Depends(get_db)):
     return await crud.get_city_list(db=db)
 
 
-@router.get("/cities/{city_id}/", response_model=schemas.City)
+@router.get("/cities/{city_id}", response_model=schemas.City)
 async def detail_city(city_id: int, db: AsyncSession = Depends(get_db)):
     return await crud.get_city_by_id(db=db, id=city_id)
 
 
-@router.put("/cities/{city_id}/", response_model=schemas.City)
+@router.put("/cities/{city_id}", response_model=schemas.City)
 async def update_city(
         city_id: int,
         city: schemas.CityUpdate,
@@ -32,7 +32,7 @@ async def update_city(
     return await crud.put_city(db=db, id=city_id, city=city)
 
 
-@router.delete("/cities/{city_id}/")
+@router.delete("/cities/{city_id}")
 async def delete_city(
         city_id: int,
         db: AsyncSession = Depends(get_db)
